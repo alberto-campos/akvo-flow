1b96503ecaefdc8ac58e50dd4740bf1f97b49f53
Mark Tiele Westra <m.t.westra@akvo.org>
2013-11-15 17:22:59 +0100

    Issue #363 - client side map code

--- Dashboard/app/js/lib/controllers/maps-controllers-common.js
+++ Dashboard/app/js/lib/controllers/maps-controllers-common.js
@@ -10,19 +10,85 @@ FLOW.placemarkController = Ember.ArrayController.create({
   content: null,
   map: null,
   overlays: Ember.A(),
+  geocellCache: [],
+  currentGcLevel: null,
+
 
   populate: function () {
-    this.set('content', FLOW.store.findAll(FLOW.Placemark));
+    //this.set('content', FLOW.store.findAll(FLOW.Placemark));
   },
 
   populateMap: function () {
-    if (this.content.get('isUpdating') === false) {
+    var gcLevel, placemarks;
+    if (this.content.get('isLoaded') === true) {
+      console.log('clearing overlays');
       this.clearOverlays();
-      this.get('content').forEach(function (placemark) {
+      gcLevel = this.get('currentGcLevel');
+      
+      // filter placemarks
+      placemarks = FLOW.store.filter(FLOW.Placemark, function(item){
+        return item.get('level') == gcLevel;
+      });
+
+      placemarks.forEach(function (placemark) {
         this.addMarker(placemark);
       }, this);
     }
-  }.observes('this.content.isUpdating'),
+  }.observes('this.content.isLoaded'),
+
+  adaptMap: function(bestBB, zoomlevel){
+    console.log('--------------------------- adapting the map --------------------');
+    var bbString = "", gcLevel, listToRetrieve = [];
+    // determine the level we want to show
+    if (zoomlevel < 5){
+      gcLevel = 2;
+    } else if (zoomlevel < 7) {
+      gcLevel = 3;
+    } else if (zoomlevel < 9) {
+      gcLevel = 4;
+    } else if (zoomlevel < 11) {
+      gcLevel = 5;
+    } else {
+      gcLevel = 0;
+    }
+    this.set('currentGcLevel',gcLevel);
+    console.log('current gclevel:',gcLevel, ', zoomlevel:', zoomlevel);
+
+    // on zoomlevel 2, the map repeats itself, leading to wrong results
+    // therefore, we force to download the highest level on all the world.
+    console.log('bestBB:',bestBB);
+    if (zoomlevel == 2) {
+    	bestBB = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    }
+    
+     // see if we already have it in the cache
+     // TODO this is not optimal at high zoom levels, as we will already have loaded the same points on a level before
+    for (var i = 0; i < bestBB.length; i++){
+      if (this.get('geocellCache').indexOf(bestBB[i]+"-"+gcLevel) < 0 ) {
+        listToRetrieve.push(bestBB[i]);
+        this.get('geocellCache').push(bestBB[i] + "-" + gcLevel);
+      }
+    }
+
+    // pack best bounding box values in a string
+    listToRetrieve.forEach(function(item){
+      bbString += item + ",";
+    });
+    // delete last comma
+    bbString = bbString.substring(0, bbString.length - 1);
+
+    // go get it in the datastore
+    // when the points come in, populateMap will trigger and place the points
+    if (!Ember.empty(bbString)){
+      console.log('--------- database query on ', bbString, ", zoomlevel:", gcLevel);
+      this.set('content',FLOW.store.findQuery(FLOW.Placemark,
+        {bbString: bbString, gcLevel: gcLevel}));
+    } else {
+    	// we might have stuff in cache, so draw anyway
+    	console.log('nothing to fetch, all comes from cache');
+    	this.populateMap();
+    }
+  },
 
   clearOverlays: function () {
     this.overlays.map(function (marker) {
@@ -32,12 +98,35 @@ FLOW.placemarkController = Ember.ArrayController.create({
   },
 
   addMarker: function (placemark) {
-    var marker, map, coordinate;
-
-    coordinate = new google.maps.LatLng(placemark.get('latitude'),
-      placemark.get('longitude'));
+    var marker, map, coordinate, div ,count, icon = 1, mapLoc;
+    coordinate = new google.maps.LatLng(placemark.get('latitude'),placemark.get('longitude'));
     map = this.get('map');
+    
+    if (placemark.get('level') > 0){
+      count = placemark.get('count');
+      
+      if (count < 10) {
+        icon = 3;
+      } else if (count < 100) {
+        icon = 2;
+      }
+ 
+      div = document.createElement('DIV');
+      div.innerHTML = '<div class="cluster-marker-' + icon + '">' + count + '</div>';
+      marker = new RichMarker({
+          position: coordinate,
+          map: map,
+          draggable: false,
+          flat: true,
+          content: div
+          });
+      
+      placemark.set('marker', marker);
+      this.get('overlays').pushObject(marker);
+      return marker;
+    } 
 
+    // if we are here, we have a normal placemark.
     marker = new google.maps.Marker({
       position: coordinate,
       map: map,
